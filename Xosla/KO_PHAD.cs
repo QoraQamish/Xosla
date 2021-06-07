f944f1557715a2d1e3e79ee236718d04b8cc509a
KamolHasanov <khasanovkamol3834@gmail.com>
2021-06-07 11:24:33 +0500

    Add project files.

---
+++ Xosla/KO_PHAD.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryClass
+{
+    public class KO_PHAD
+    {
+        public void hisobla(string argument, double darajasi, double n)
+        {
+            double koy = 1;
+            if (n < darajasi)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    koy *= darajasi;
+                    darajasi -= 1;
+                }
+                Console.WriteLine($"{koy}{argument}^{darajasi}");
+            }
+            else if (n == darajasi)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    koy *= darajasi;
+                    darajasi -= 1;
+                }
+                Console.WriteLine($"{koy}");
+            }
+            else if (n > darajasi)
+                Console.WriteLine("0");
+        }
+        public void hisobla(double koyefsent, string argument, double darajasi, double n)
+        {
+            if (n < darajasi)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    koyefsent *= darajasi;
+                    darajasi -= 1;
+                }
+                Console.WriteLine($"{koyefsent}{argument}^{darajasi}");
+            }
+            else if (n == darajasi)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    koyefsent *= darajasi;
+                    darajasi -= 1;
+                }
+                Console.WriteLine($"{koyefsent}");
+            }
+            else if (n > darajasi)
+                Console.WriteLine("0");
+        }
+    }
+}
---
+++ Xosla/Program.cs
@@ -0,0 +1,31 @@
+﻿using DirectoryClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xosla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Birxadli funksiya kiriting");
+            Console.Write("koyefsentini kiting >> ");
+            double koy = double.Parse(Console.ReadLine());
+
+            Console.Write("argumentini kiting >> ");
+            string argument = Console.ReadLine();
+
+            Console.Write("darajasini kiting >> ");
+            double daraja = double.Parse(Console.ReadLine());
+
+            Console.Write("necha marta hosila olasiz >> ");
+            double n = double.Parse(Console.ReadLine());
+
+            KO_PHAD ob = new KO_PHAD();
+            ob.hisobla(koy, argument, daraja, n);
+        }
+    }
+}
